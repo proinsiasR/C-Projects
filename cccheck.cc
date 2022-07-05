75e23293688b4a42bef44e7e13ac70414a72baf5
proinsiasR <85370828+proinsiasR@users.noreply.github.com>
2022-07-05 11:41:43 -0400

    Add files via upload

---
+++ cccheck.cc
@@ -0,0 +1,41 @@
+// C++ program to implement Luhn algorithm
+#include <bits/stdc++.h>
+using namespace std;
+
+// Returns true if given card number is valid
+bool checkLuhn(const string& cardNo)
+{
+  int len = cardNo.length();
+
+  int nSum = 0, isSecond = false;
+  for (int i = len - 1; i >= 0; i--) {
+
+    int d = cardNo[i] - '0';
+
+    if (isSecond == true)
+      d = d * 2;
+
+    // We add two digits to handle
+    // cases that make two digits after
+    // doubling
+    nSum += d / 10;
+    nSum += d % 10;
+
+    isSecond = !isSecond;
+  }
+  return (nSum % 10 == 0);
+}
+
+// Driver code
+int main()
+{
+  string cardNo;
+  cout << "This will use the Luhn Algorithm to check a credit card number. \nEnter card number or type 'exit' to quit: " << endl;
+  cin >> cardNo;
+  if(cardNo == "exit");
+  else if (checkLuhn(cardNo))
+    printf("This is a valid card");
+  else
+    printf("This is not a valid card");
+  return 0;
+}
---
+++ checklastdigit.cc
@@ -0,0 +1,37 @@
+
+// C++ program to find check digit using Lunn Algorithm
+#include <bits/stdc++.h>
+using namespace std;
+
+int checkDigit(const string& cardNo)
+{
+  int len = cardNo.length();
+
+  int nSum = 0, isSecond = true;
+  for (int i = len - 1; i >= 0; i--) {
+
+    int d = cardNo[i] - '0';
+
+    if (isSecond == true)
+      d = d * 2;
+
+    nSum += d / 10;
+    nSum += d % 10;
+
+    isSecond = !isSecond;
+  }
+  int lastDigit = ((int)10 -(nSum%10));
+  return lastDigit;
+}
+
+// Driver code
+int main()
+{
+  string cardNo;
+  cout << "This will use the Luhn Algorithm to check the last digit of a credit card number. \nEnter card number or type 'exit' to quit: " << endl;
+  cin >> cardNo;
+  if(cardNo == "exit");
+  else
+    cout << checkDigit(cardNo) << endl;
+  return 0;
+}
